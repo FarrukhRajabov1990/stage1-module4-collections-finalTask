c8b025846c61447d8535dca144d8055a45e68e31
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-10-12 13:21:19 +0500

    Initial commit

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,26 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+
+        List<String> projectsOfTheDeveloper = new ArrayList<>();
+
+        for (String project : projects.keySet()) {
+            if (projects.get(project).contains(developer)) {
+                projectsOfTheDeveloper.add(project);
+            }
+        }
+
+        projectsOfTheDeveloper.sort((v1, v2) -> {
+            if (v1.length() != v2.length()){
+                return Integer.compare(v2.length(), v1.length());
+            } else {
+                return v2.compareTo(v1);
+            }
+        });
+
+        return projectsOfTheDeveloper;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,28 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+
+        Map<Integer, Set<String>> result = new HashMap<>();
+
+        int length;
+        for (String word : sourceMap.keySet()) {
+            length = word.length();
+            if (sourceMap.containsValue(length)) {
+                Set<String> words = new HashSet<>();
+                for (String innerWord : sourceMap.keySet()){
+                    if (innerWord.length() == length) {
+                        words.add(innerWord);
+                    }
+                }
+                result.put(length, words);
+            }
+        }
+        return result;
     }
 }
